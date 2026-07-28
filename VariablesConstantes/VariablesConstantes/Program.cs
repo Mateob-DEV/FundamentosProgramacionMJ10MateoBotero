9af0494a2d99f01e127c2e31669b0944268ae571
B09S104est <B09S104est@MEDAULAS.UPB>
2026-07-28 11:16:24 -0500

    Este es mi primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui codigo fuente
+            string nombre="Mateo Botero";
+            Console.WriteLine(nombre + " " + "Curso Fundamentos de programacion");
+        }
+    }
+}
